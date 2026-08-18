1c920144b3b604d51310d6b6f0249d927c10f023
Rishi <rishi_j@icloud.com>
2026-08-18 11:43:26 +0100

    Initial Commit

---
+++ Chess/ChessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChessApp.swift
+//  Chess
+//
+//  Created by Rishi Jansari on 18/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct ChessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Chess/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Chess
+//
+//  Created by Rishi Jansari on 18/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
